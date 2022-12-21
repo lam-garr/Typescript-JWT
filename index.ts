bc1b4d1f1f3cdf134b9c095ff636ebce3e9b73bb
Garrett <lamgarr@outlook.com>
2022-12-20 21:38:05 -0800

    Add express

--- index.ts
+++ index.ts
@@ -1,1 +1,14 @@
-console.log("hello wordl")
+import express, { Express, Request, Response, NextFunction } from "express";
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+const app: Express = express();
+
+app.get("/", (req: Request, res: Response) => {
+    res.send("Hello World");
+})
+
+app.listen(process.env.PORT, () => {
+    console.log(`Listening on port ${process.env.PORT}`)
+})
